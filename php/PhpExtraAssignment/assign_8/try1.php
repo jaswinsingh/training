c63d6bc2a125fa1402ac3b5fa4f29523137d8b64
jaswinsingh <jaswin.singh@innoraft.com>
2020-02-11 00:13:57 +0530

    extra assignment home

--- php/PhpExtraAssignment/assign_8/try1.php
+++ php/PhpExtraAssignment/assign_8/try1.php
@@ -1,55 +1,107 @@
 <?php
 
-class  college
-{
-  public $id,$name,$details=[];;
-  function __contructor($id,$name)
-  {
-    $this->id=$id;
-    $this->name=$name;
-    $this->details=[];
-  }
-  function getid()
-  {
-    return $this->id;
-  }
-  function getname()
-  {
-    return $this->name;
-  }
-}
+    include 'data.php';
+    class college_details
+    {
+        public $id;
+        public $name;
+        public $details=[];
+        function __construct($id,$name)
+        {
+            $this->id=$id;
+            $this->name=$name;
+            $this->details=[];
+        }
+        function getid() {
+            return $this->id;
+        }
+        function getname() {
+            return $this->name;
+        }
+    }
+    class document_details
+    {
+        public $name;
+        public $type;
+        public $college;
+        public $status;
+        function __construct($name,$type,$college,$sent)
+        {
+            $this->name=$name;
+            $this->type=$type;
+            $this->college=$college;
+            $this->status=$sent;
+        }
+        function getname()
+        {
+            return $this->name;
+        }
+        function gettype()
+        {
+            return $this->type;
+        }
+        function getcollege()
+        {
+            return $this->college;
+        }
+        function getsent()
+        {
+            return $this->sent;
+        }
+    }
 
-class  document
-{
-  public $doc_name,$doc_type,$doc_college,$sent;
-  function __contructor($doc_name,$doc_type,$doc_college,$sent)
-  {
-    $this->doc_name=$doc_name;
-    $this->doc_college=$doc_college;
-    $this->doc_type=$doc_type;
-    $this->sent=$sent;
-  }
-  function getname()
-       {
-           return $this->name;
-       }
-       function gettype()
-       {
-           return $this->type;
-       }
-       function getcollege()
-       {
-           return $this->college;
-       }
-       function getsent()
-       {
-           return $this->sent;
-       }
-}
+    $clg_dt=[];
+    $doc_dt=[];
+    //college objects
+    foreach($clg_data as $key => $value)
+    {
+        $obj=new college_details($value['id'],$value['name']);
+        $clg_dt[] = $obj;
 
-$college=array();
-for ($i=0; $i < 5; $i++) {
-$college[]=$i;
-}
+    }
+    //document objects
+    foreach($doc_data as $key1 => $value1)
+    {
+        $obj1=new document_details($value1['name'],$value1['type'],$value1['college'],$value1['status']);
+        $doc_dt[]=$obj1;
 
- ?>
+        merge($value1['college'],$value1['name'],$value1['type'],$value1['status'],$key1);
+    }
+
+     function merge($c,$n,$t,$s,$key1)
+     {
+        global $clg_dt;
+        foreach($clg_dt as $key => $value)
+        {
+
+            if(($c) == ($value->id))
+            {
+                $value->details[$key1]['doc-name']=$n;
+                $value->details[$key1]['doc-type']=$t;
+                if ($s)
+                  $value->details[$key1]['doc-status']="Success";
+                else {
+                  $value->details[$key1]['doc-status']="Fail";
+                }
+            }
+            if($c=='')
+            {
+                $value->details[$key1]['doc-name']=$n;
+                $value->details[$key1]['doc-type']=$t;
+                if ($s)
+                  $value->details[$key1]['doc-status']="Success";
+                else {
+                  $value->details[$key1]['doc-status']="Fail";
+                }
+            }
+
+            }
+        }
+
+        echo "<pre>";
+        // //print_r($clg_dt);
+        // //print_r($doc_dt);
+        print_r($clg_dt);
+        print_r($doc_dt);
+        echo "</pre>";
+        // fun();
---
+++ php/PhpExtraAssignment/assign_9/class.php
@@ -0,0 +1,12 @@
+<?php
+class student {
+  public $student_id,$student_name,$student_dob,$student_grade;
+  function __constructor($student_id,$student_name,$student_dob,$student_grade)
+  {
+    $this->student_id=$student_id;
+    $this->student_name=$student_name;
+    $this->student_dob=$student_dob;
+    $this->student_grade=$student_grade;
+  } 
+}
+ ?>
