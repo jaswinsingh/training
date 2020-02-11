1294db40c66e05f17a997a8ce248467cbf019bf9
jaswin.singh <jaswin.singh@innoraft.com>
2020-02-11 18:30:54 +0530

    assignment 9

--- php/PhpExtraAssignment/assign_9/class.php
+++ php/PhpExtraAssignment/assign_9/class.php
@@ -1,12 +1,116 @@
 <?php
+include 'data.php';
+
 class student {
-  public $student_id,$student_name,$student_dob,$student_grade;
-  function __constructor($student_id,$student_name,$student_dob,$student_grade)
+  public $id,$name,$DoB,$grade;
+  public $mark=[];
+
+  function __construct($id,$name,$DoB,$grade,$x)
   {
-    $this->student_id=$student_id;
-    $this->student_name=$student_name;
-    $this->student_dob=$student_dob;
-    $this->student_grade=$student_grade;
-  } 
+    $this->id=$id;
+    $this->name=$name;
+    $this->DoB=$DoB;
+    $this->grade=$grade;
+    $this->mark=[$x];
+  }
 }
+
+class subject{
+  public $Subject,$S_code,$mm;
+  function __construct($Subject,$S_code,$mm)
+  {
+    $this->Subject=$Subject;
+    $this->S_code=$S_code;
+    $this->mm=$mm;
+  }
+}
+
+
+function setmarks($id)
+{
+  global $marks;
+  foreach ($marks as $key => $value) {
+    if ($key==$id) {
+      // $mark[]=$value;
+      return $value;
+    }
+  }
+}
+
+$st=[];
+$sub=[];
+foreach ($student as $key => $value) {
+// echo $value['id'],$value['name'],$value['DoB'],$value['grade']."<br>";
+$x=setmarks($value['id']);
+$obj=new student($value['id'],$value['name'],$value['DoB'],$value['grade'],$x);
+
+$st[]=$obj;
+
+}
+
+
+
+
+
+
+
+
+
+foreach ($subject as $key => $value) {
+  // $obj2=new grade($key);
+  // $sub[]=$obj2;
+  foreach ($value as $k1 => $v1) {
+    // echo $v1['Subject'];
+    // echo $key;/
+
+    $obj1=new subject($v1['Subject'],$v1['S_code'],$v1['mm']);
+
+    $sub[$key][]=$obj1;
+    // print_r( $obj1);
+  }
+}
+
+class test{
+  function showSubject($var)
+  {
+      global $sub;
+      foreach ($sub as $key => $value) {
+        if ($var==$key) {
+          // code...
+
+        foreach ($value as $k1 => $v1) {
+          echo "Subject[‘name’] = ".$v1->Subject."<br>";
+            echo "Subject[‘code’] = ".$v1->S_code."<br>";
+              echo "Subject[mm] = ".$v1->mm."<br>";
+              echo "<br>";
+        }
+        }
+      }
+  }
+
+function showMarks($id)
+{
+  global $st;
+  foreach ($st as $key => $value) {
+    print_r($value);
+  }
+}
+
+}
+
+echo "<pre>";
+// print_r($subject);
+// print_r($marks);
+print_r($st);
+//  print_r($sub);
+//  print_r($student);
+//
+echo "</pre>";
+
+
+$test1=new test();
+$test1->showMarks(101);
+
+
+
  ?>
--- php/PhpExtraAssignment/assign_9/data.php
+++ php/PhpExtraAssignment/assign_9/data.php
@@ -4,25 +4,88 @@ $student=[
       'id'=>'st1',
       'name'=>'John',
       'DoB'=>'1580812642',
-      'grade'=>12
+      'grade'=>10
     ],
     [
       'id'=>'st2',
       'name'=>'Marry',
-      'DoB'=>'1752007017',
+      'DoB'=>'1152975017',
       'grade'=>12
     ],
     [
-      'id'=>'st1',
-      'name'=>'John',
-      'DoB'=>'1580812642',
-      'grade'=>12
+      'id'=>'st3',
+      'name'=>'steve',
+      'DoB'=>'1613795410',
+      'grade'=>10
     ],
-    [
-      'id'=>'st1',
-      'name'=>'John',
-      'DoB'=>'1580812642',
-      'grade'=>12
-    ],
+    // [
+    //   'id'=>'st4',
+    //   'name'=>'John',
+    //   'DoB'=>'1429497549',
+    //   'grade'=>10
+    // ],
 ];
+
+
+
+
+$subject=[
+
+          10=>[
+                [
+                  'Subject'=>'Maths',
+                  'S_code'=>'M101',
+                  'mm'=>20
+                ],
+                [
+                  'Subject'=>'English',
+                  'S_code'=>'HU101',
+                  'mm'=>15
+                ],
+                [
+                  'Subject'=>'Hindi',
+                  'S_code'=>'HI101',
+                  'mm'=>13
+                ]
+              ],
+
+          12=>[
+                [
+                  'Subject'=>'Maths',
+                  'S_code'=>'M201',
+                  'mm'=>25
+                ],
+                [
+                  'Subject'=>'English',
+                  'S_code'=>'HU201',
+                  'mm'=>20
+                ],
+                [
+                  'Subject'=>'Computer',
+                  'S_code'=>'CS201',
+                  'mm'=>20
+                ]
+              ]
+
+            ];
+
+    $marks=[
+
+                'st1'=>[
+                          'HU101'=>55,
+                          'M101'=>7,
+                          'HI101'=>9,
+                      ],
+                'st2'=>[
+                          'HU201'=>15,
+                          'M201'=>21,
+                          'CS201'=>17,
+                      ],
+                      'st3'=>[
+                                'HU101'=>10,
+                                'M101'=>12,
+                                'HI101'=>11,
+                            ],
+
+          ];
  ?>
