4d3f39973771a34df3b6f0182398255029752372
jaswin.singh <jaswin.singh@innoraft.com>
2020-03-02 13:01:55 +0530

    CSS changed of front login page {changed all centered placeholder to left aligned ,added labels to better readability}

--- php/PhpExtraAssignment/blog_mvc/view/loginHTML.php
+++ php/PhpExtraAssignment/blog_mvc/view/loginHTML.php
@@ -2,8 +2,8 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <link rel="stylesheet" href="/internship/php/PhpExtraAssignment/blog_mvc/view/index.css?v=1">
-    <link rel="stylesheet" href="/internship/php/PhpExtraAssignment/blog_mvc/view/login.css?v=2">
+    <link rel="stylesheet" href="/internship/php/PhpExtraAssignment/blog_mvc/view/index.css?v=2">
+    <link rel="stylesheet" href="/internship/php/PhpExtraAssignment/blog_mvc/view/login.css?v=1">
     <link href="https://fonts.googleapis.com/css?family=Mukta+Malar:200,400,700&display=swap" rel="stylesheet">
     <script src="https://www.google.com/recaptcha/api.js" async defer></script>
     <title></title>
@@ -13,12 +13,20 @@
     <div class="wrapper">
     <div class="loginForm">
       <form class="login" action="" method="post">
-        <input type="text" name="userName" value="" placeholder="USERNAME" required>
-        <input type="password" name="password" value="" placeholder="PASSWORD" required>
+
+          <label for="userName">Username</label>
+          <input type="text" name="userName" value="" placeholder="Robert" required>
+
+
+            <label for="password">password</label>
+          <input type="password" name="password" value="" placeholder="PASSWORD" required>
+
+
+
         <div class="g-recaptcha" data-sitekey="6LdE0NwUAAAAADlW_4jIUaZCVcCf760M21KPzJ5K"></div>
         <input type="submit" name="submit" value="SUBMIT">
         <div id="formFooter">
-            <a class="underlineHover" href="index.php">TEMPORARY LOGIN</a>
+            <!-- <a class="underlineHover" href="index.php">TEMPORARY LOGIN</a>// -->
           <a class="underlineHover" href="http://www.jaswinsingh.com/str/register">REGISTER</a>
         </div>
         <div class="message">
